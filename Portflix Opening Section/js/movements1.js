d6720065593161e85ae12bc187b8912f9d3b5a43
Rauf Ismayilov <rauf.raymond110@gmail.com>
2021-02-11 15:29:00 +0400

    fixed response bugs

---
+++ Portflix Opening Section/js/movements1.js
@@ -0,0 +1,260 @@
+const flightPath1 = {
+    curviness: 1.25,
+    autoRotate: true,
+    values: [
+        {x: 200, y: 100},
+        {x: 250, y: 150},
+        {x: 300, y: 200},
+        {x: 500, y: 400},
+        {x: 850, y: 320},
+        {x: 900, y: 250},
+        {x: 850, y: 150},
+        {x: 200, y: 320},
+        {x: 300, y: 800},
+        {x: 400, y: 810},
+        {x: 450, y: 810},
+    ]
+}
+
+const tween1 = new TimelineLite();
+
+
+tween1.add(
+    TweenLite.to('.header-center-animated-container', 350 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        opacity: 1,
+    })
+)
+tween1.add(
+    TweenLite.to('.header-center-animated-container', 350 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        opacity: 0,
+    })
+)
+tween1.add(
+    TweenLite.to('#apple-menu', 200 ,{
+        // width: "760px"    ,
+        transform: "translateY(-240px)"
+        // bottom: "-200vh"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#rectangle', 1000 ,{
+        bezier: flightPath1,
+        // ease: Power1.easeInOut,
+        // opacity: 1,
+    })
+)
+tween1.add(
+    TweenLite.to('#rectangle', 50 ,{
+        // bezier: flightPath1,
+        // ease: Power1.easeInOut,
+        opacity: 0,
+    })
+)
+
+
+tween1.add(
+    TweenLite.to('#chrome', 70 ,{
+        // width: "760px"    ,
+        transform: "translateY(-25px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#chrome', 70,{
+        // width: "760px"    ,
+        transform: "translateY(0px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#facebook', 70 ,{
+        // width: "760px"    ,
+        transform: "translateY(-25px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#facebook', 70,{
+        // width: "760px"    ,
+        transform: "translateY(0px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#gmail', 70 ,{
+        // width: "760px"    ,
+        transform: "translateY(-25px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#gmail',70 ,{
+        // width: "760px"    ,
+        transform: "translateY(0px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#speed', 70,{
+        // width: "760px"    ,
+        transform: "translateY(-25px)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#speed', 200,{
+        // width: "760px"    ,
+        transform: "translateY(-35px) scale(3.5)"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+tween1.add(
+    TweenLite.to('#apple-menu', 100 ,{
+        // width: "760px"    ,
+        transform: "translateY(240px)"
+        // bottom: "-200vh"
+            // backgroundImage: 'url("../img/smartphone.png")  '
+    })
+)
+
+tween1.add(
+    TweenLite.to('.phone-border', 500 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        transform: "translateY(1800px) translateX(-400px) scale(0.3)"
+    })
+)
+
+tween1.add(
+    TweenLite.to('.speedtestBCK', 500 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        opacity: 1
+    })
+)
+tween1.add(
+    TweenLite.to('.mobile-interior', 100 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        background: "none"
+    })
+)
+
+tween1.add(
+    TweenLite.to('.phone-animated-header', 500 ,{
+        transform: `translateY(-200px)`,
+        opacity: "1"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-header', 500 ,{
+        transform: `translateY(-400px)`,
+        opacity: "0"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-paragraph', 500 ,{
+        transform: `translateY(-200px)`,
+        opacity: "1"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-paragraph', 500 ,{
+        transform: `translateY(-400px)`,
+        opacity: "0"
+    })
+)
+
+tween1.add(
+    TweenLite.to('.phone-animated-header-second', 500 ,{
+        transform: `translateY(-200px)`,
+        opacity: "1"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-header-second', 500 ,{
+        transform: `translateY(-400px)`,
+        opacity: "0"
+    })
+)
+tween1.add(
+    TweenLite.to('.youtubeBCK', 400 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        opacity: 1
+    })
+)
+tween1.add(
+    TweenLite.to('.speedtestBCK', 400 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        opacity: 0
+    })
+)
+
+tween1.add(
+    TweenLite.to('.phone-border', 1500 ,{
+        // bezier: flightPath6,
+        // ease: Power1.easeInOut,
+        transform: "translateY(3650px) translateX(300px) scale(0.3)"
+    })
+)
+
+
+
+tween1.add(
+    TweenLite.to('.phone-animated-header-benefit', 500 ,{
+        transform: `translateY(-200px)`,
+        opacity: "1"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-header-benefit', 500 ,{
+        transform: `translateY(-400px)`,
+        opacity: "0"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-header-benefit-1', 500 ,{
+        transform: `translateY(-200px)`,
+        opacity: "1"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-header-benefit-1', 500 ,{
+        transform: `translateY(-400px)`,
+        opacity: "0"
+    })
+)
+
+tween1.add(
+    TweenLite.to('.phone-animated-header-benefit-2', 500 ,{
+        transform: `translateY(-200px)`,
+        opacity: "1"
+    })
+)
+tween1.add(
+    TweenLite.to('.phone-animated-header-benefit-2', 500 ,{
+        transform: `translateY(-400px)`,
+        opacity: "0"
+    })
+)
+
+
+const controller1 = new ScrollMagic.Controller();
+
+const scene1 = new ScrollMagic.Scene({
+    triggerElement: '#phone-trigger',
+    duration: 4000,
+    triggerHook: 0.2 
+
+})
+.setTween(tween1)
+// .addIndicators()
+.setPin('#phone-trigger')
+.addTo(controller1)
--- Portflix Opening Section/js/movements2.js
+++ Portflix Opening Section/js/movements2.js
@@ -1,49 +1,34 @@
-const flightPath2 = {
-    curviness: 1.25,
-    autoRotate: true,
-    values: [
-        {x: 100, y: 400},
-        {x: 150, y: 450},
-        {x: 200, y: 500},
-        {x: 400, y: 700},
-        {x: 650, y: 720},
-        {x: 700, y: 650},
-        {x: 650, y: 550},
-        {x: 100, y: 720},
-        {x: 200, y: 990},
-        {x: 300, y: 990},
-        {x: 400, y: 990},
-        {x: 450, y: 990},
-        {x: 490, y: 990},
-        {x: 510, y: 990},
-        {x: 530, y: 990},
-        {x: 550, y: 990},
-        {x: 580, y: 990},
-        {x: 600, y: 990},
-        {x: 620, y: 990}
-    ]
-}
+// const flightPath1 = {
+//     curviness: 1.25,
+//     autoRotate: true,
+//     values: [
+//         {x: 200, y: 400},
+//         {x: 250, y: 450},
+//         {x: 300, y: 500},
+//         {x: 500, y: 700},
+//         {x: 850, y: 620},
+//         {x: 900, y: 550},
+//         {x: 850, y: 450},
+//         {x: 200, y: 620},
+//         {x: 300, y: 1190},
+//         {x: 400, y: 1290},
+//     ]
+// }
 
 const tween2 = new TimelineLite();
 
-tween2.add(
-    TweenLite.to('#rectangle', 1 ,{
-        bezier: flightPath2,
-        ease: Power1.easeInOut
-    })
-)
+
+
 
 const controller2 = new ScrollMagic.Controller();
 
 const scene2 = new ScrollMagic.Scene({
-    triggerElement: '.animation',
-    // animasia muddeti
-    duration: 650,
-    // triggerin yeri y axis
-    triggerHook: 0  
+    triggerElement: '#text-trigger',
+    duration: 1200,
+    triggerHook: 0.6
 
 })
 .setTween(tween2)
-// .addIndicators()
-// .setPin('.animation')
+.addIndicators()
+.setPin('#text-trigger')
 .addTo(controller2)
